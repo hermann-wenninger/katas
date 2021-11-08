651ba98e22abcc410e3e232916a89093b3a895e2
hermann wenninger <hermann23@vivaldi.net>
2021-11-08 19:13:41 +0100

    #journal

--- sqirr.js
+++ sqirr.js
@@ -90,10 +90,58 @@ var JOURNAL = [
   {"events":["bread","brushed teeth","television","weekend"],"squirrel":false},
   {"events":["cauliflower","peanuts","brushed teeth","weekend"],"squirrel":false}
 ];
+var journal = [];
 
-// This makes sure the data is exported in node.js —
-// `require('./path/to/journal.js')` will get you the array.
-if (typeof module != "undefined" && module.exports && (typeof window == "undefined" || window.exports != exports))
-  module.exports = JOURNAL;
-if (typeof global != "undefined" && !global.JOURNAL)
-  global.JOURNAL = JOURNAL;
+function addEntry(events, squirrel) {
+  journal.push({events, squirrel});
+}
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[0] + table[2]));
+}
+
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i], index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+function journalEvents(JOURNAL) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+
+function max(...numbers) {
+  let result = -Infinity;
+  for (let number of numbers) {
+    if (number > result) result = number;
+  }
+  return result;
+}
+
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
